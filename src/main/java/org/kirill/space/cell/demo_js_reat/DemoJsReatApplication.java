f72c2a0f37363f066f0097eccfcfc381c6a1feab
Kirill Kozlov <space_cell@proton.me>
2024-12-02 21:42:56 +0500

    Init project

---
+++ src/main/java/org/kirill/space/cell/demo_js_reat/DemoJsReatApplication.java
@@ -0,0 +1,13 @@
+package org.kirill.space.cell.demo_js_reat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJsReatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJsReatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kirill/space/cell/demo_js_reat/DemoJsReatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kirill.space.cell.demo_js_reat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJsReatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
